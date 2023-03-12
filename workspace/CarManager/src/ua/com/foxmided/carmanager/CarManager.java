36dc5fa34bd5efa6f93804ecff5b2ab1f7b9c7d1
viktau <tataurov@kpnu.km.ua>
2023-03-12 22:39:21 +0200

    Create CarManager class

---
+++ workspace/CarManager/src/ua/com/foxmided/carmanager/CarManager.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ua.com.foxmided.carmanager;
+
+/**
+ * @author Admin
+ *
+ */
+public class CarManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
