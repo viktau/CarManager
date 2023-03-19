b222b95823c21f25522e81e34c78edd8935f87b7
viktau <tataurov@kpnu.km.ua>
2023-03-19 23:17:55 +0200

    Create method "replace","toUpperCase","valueOf"

--- workspace/CarManager/src/ua/com/foxmided/carmanager/Color.java
+++ workspace/CarManager/src/ua/com/foxmided/carmanager/Color.java
@@ -1,5 +1,12 @@
 package ua.com.foxmided.carmanager;
 
 public enum Color {
+	WHITE , GREEN, BLACK, BLUE, ORANGE;
+
+	Color valueOf(Color color) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
--- workspace/CarManager/src/ua/com/foxmided/carmanager/Motorcycle.java
+++ workspace/CarManager/src/ua/com/foxmided/carmanager/Motorcycle.java
@@ -10,6 +10,8 @@ public class Motorcycle {
 	Color color;
 	String engineType;
     boolean isReadyToDrive;
+    private int distance =0;
+    
 	public Motorcycle(String name, int yearOfProdaction, int price, int weight, Color color, String engineType,
 			boolean isReadyToDrive) {
 		super();
@@ -17,15 +19,47 @@ public class Motorcycle {
 		this.yearOfProdaction = yearOfProdaction;
 		this.price = price;
 		this.weight = weight;
-		this.color = color;
+		// this.color = color;
+		color = color.replace(" ", "");
+		color = color.toUpperCase();
+		this.color = Color.valueOf(color);
 		this.engineType = engineType;
 		this.isReadyToDrive = isReadyToDrive;
+		
+		//this.color = Color.valueOf(color); 
 	}
+
+	public void addDistance (int additionalDistance){
+		distance +=additionalDistance;
+	}
+	public int getDistance() {
+		return distance;
+	}
+
+	//Создать метод repair в классе Motorcycle, который будет чинить сломанный мотоцикл.
+	public void repair() {
+	if (isReadyToDrive ==false) 
+		isReadyToDrive =true;
+	}
+	
+	//Создать метод destroyEngine, который будет ломать мотоцикл после 200 тыс. пробега
+
+	public void destroyEngine () {
+		if (distance>=200000)
+		isReadyToDrive=false;	
+	}
+// Создать метод, который будет красить мотоцикл
+	public Color setNewColor (String newColor) {
+		newColor = newColor.replace(" ", "");
+		newColor = newColor.toUpperCase();
+		return this.color = Color.valueOf(newColor);
+	}
+
 	@Override
 	public String toString() {
 		return "Motorcycle [name=" + name + ", yearOfProdaction=" + yearOfProdaction + ", price=" + price + ", weight="
 				+ weight + ", color=" + color + ", engineType=" + engineType + ", isReadyToDrive=" + isReadyToDrive
-				+ "]";
+				+ ", distance=" + distance + "]";
 	}
 	// створення методу equals для порівняння обєктів класу
 	@Override
